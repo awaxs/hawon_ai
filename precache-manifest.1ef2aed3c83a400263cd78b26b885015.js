5e8f4f9713934200b337502e2c426305e8f6680e
Hawon_test <jeongts@jbnu.ac.kr>
2023-12-15 04:38:28 +0900

    Updates

---
+++ precache-manifest.1ef2aed3c83a400263cd78b26b885015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d4fbe639adfe735cfe5f966a48b47c",
+    "url": "/hawon_ai/index.html"
+  },
+  {
+    "revision": "5b3b9ac9f0c0454dfcdb",
+    "url": "/hawon_ai/static/css/main.be5ed728.chunk.css"
+  },
+  {
+    "revision": "4ed525572b5ae564d0f0",
+    "url": "/hawon_ai/static/js/2.a695d15d.chunk.js"
+  },
+  {
+    "revision": "5b3b9ac9f0c0454dfcdb",
+    "url": "/hawon_ai/static/js/main.185a872f.chunk.js"
+  },
+  {
+    "revision": "8edeaafc6e7bdfb4a898",
+    "url": "/hawon_ai/static/js/runtime~main.3b0239d1.js"
+  },
+  {
+    "revision": "43327df2d26ec39d75440f5caf407074",
+    "url": "/hawon_ai/static/media/AI.43327df2.jpg"
+  }
+]);
